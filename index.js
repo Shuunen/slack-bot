0a10044843d22f8678fd19e8f63550b127d5c62a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2017-06-02 11:44:24 +0200

    humanized a bit

--- index.js
+++ index.js
@@ -9,13 +9,17 @@
 
 // basic mods
 const fetch = require('node-fetch')
+const _ = require('underscore');
+const Chance = require('chance');
+const chance = new Chance();
+const shuffle = require('shuffle-array');
 
 // get secret token
 const fs = require('fs')
 let token = fs.readFileSync('./token.conf', 'utf8')
 if (!token) {
-    console.log('please set a slack token in a token.conf file')
-    return
+  console.log('please set a slack token in a token.conf file')
+  return
 }
 token = token.replace('\n', '')
 
@@ -24,6 +28,26 @@ const Botkit = require('botkit')
 let bot = Botkit.slackbot({ debug: false })
 bot.spawn({ token: token }).startRTM()
 
+/*                            __      ___    __       
+  |__| |  |  |\/|  /\  |\ | |  /  /\   |  | /  \ |\ | 
+  |  | \__/  |  | /~~\ | \| | /_ /~~\  |  | \__/ | \| */
+/*
+  Tools
+*/
+const pick = (arr) => shuffle(arr)[0]
+const firstCap = (str) => str.charAt(0).toUpperCase() + str.slice(1)
+/* 
+  Vocabulary
+*/
+const endPunctuation = (withFun) => pick(withFun ? [' :)', ' :D'] : ['.', '.', '', '...'])
+const greetings = ['salut ', 'hello ', 'hey ']
+const greeting = () => pick(greetings)
+/*
+  Interactions
+*/
+const say = (str, withFun) => firstCap(str + endPunctuation(withFun))
+const welcome = (who) => say(greeting() + who, true)
+
 /* ___  __        __       ___    __       
   |__  |  \ |  | /  `  /\   |  | /  \ |\ | 
   |___ |__/ \__/ \__, /~~\  |  | \__/ | \|  */
@@ -31,15 +55,15 @@ bot.spawn({ token: token }).startRTM()
   Hello
 */
 bot.hears('ça va', ['ambient'], (bot, message) => {
-    bot.reply(message, 'Nickel et toi ?')
+  bot.reply(message, 'Nickel et toi ?')
 })
 /*
   Time
 */
 bot.hears('quelle heure', ['ambient'], (bot, message) => {
-    let date = new Date()
-    let time = date.getHours() + ':' + date.getMinutes()
-    bot.reply(message, 'il est ' + time)
+  let date = new Date()
+  let time = date.getHours() + ':' + date.getMinutes()
+  bot.reply(message, 'il est ' + time)
 })
 /*
   Weather
@@ -47,10 +71,18 @@ bot.hears('quelle heure', ['ambient'], (bot, message) => {
 let forecasts = {}
 fetch('http://www.prevision-meteo.ch/services/json/nantes').then(res => res.json()).then(json => forecasts = json)
 bot.hears('quel temps', ['ambient'], (bot, message) => {
-    console.log('complete message was : ', message.text)
-    let today = (message.text.indexOf('demain') === -1)
-    let forecast = today ? forecasts.fcst_day_0 : forecasts.fcst_day_1
-    let response = today ? 'aujourd\'hui ' : 'demain '
-    response += 'il fera entre ' + forecast.tmin + '° et ' + forecast.tmax + '°'
-    bot.reply(message, response)
+  console.log('complete message was : ', message.text)
+  let today = (message.text.indexOf('demain') === -1)
+  let forecast = today ? forecasts.fcst_day_0 : forecasts.fcst_day_1
+  let response = today ? 'aujourd\'hui ' : 'demain '
+  response += 'il fera entre ' + forecast.tmin + '° et ' + forecast.tmax + '°'
+  bot.reply(message, response)
 })
+/*
+  Welcome 
+*/
+bot.hears(greetings, ['mention', 'direct_mention'], (bot, message) => {
+  // console.log('got message', message)
+  bot.reply(message, welcome(message.user))
+})
+
