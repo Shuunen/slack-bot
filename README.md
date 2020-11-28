553e4252fc24da39ac0bc8bc3e01ec889f227b9b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-11-28 13:01:46 +0100

    last message

--- README.md
+++ README.md
@@ -8,7 +8,17 @@
 
 > Annoying slack bot
 
+## About
+
+This was made on a launch break to test building a chat bot and also be able to mess with my coworkers, feel free to judge me, but only the FSM can.
+
+## Now
+
+Made in 2017, source code might be deprecated, beware !
+
 ## Thanks
 
+- [Github](https://github.com) : for all their great work year after year, pushing OSS forward
+- [Repo-checker](https://github.com/Shuunen/repo-checker) : eslint cover /src code and this tool the rest ^^
 - [Shields.io](https://shields.io) : nice looking badges to be proud of
 - [Travis-ci.org](https://travis-ci.org) : for providing free continuous deployments
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /*
 ███████╗██╗      █████╗  ██████╗██╗  ██╗     ██████╗  ██████╗ ████████╗
 ██╔════╝██║     ██╔══██╗██╔════╝██║ ██╔╝     ██╔══██╗██╔═══██╗╚══██╔══╝
-███████╗██║     ███████║██║     █████╔╝█████╗██████╔╝██║   ██║   ██║   
-╚════██║██║     ██╔══██║██║     ██╔═██╗╚════╝██╔══██╗██║   ██║   ██║   
-███████║███████╗██║  ██║╚██████╗██║  ██╗     ██████╔╝╚██████╔╝   ██║   
-╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═════╝  ╚═════╝    ╚═╝  
+███████╗██║     ███████║██║     █████╔╝█████╗██████╔╝██║   ██║   ██║
+╚════██║██║     ██╔══██║██║     ██╔═██╗╚════╝██╔══██╗██║   ██║   ██║
+███████║███████╗██║  ██║╚██████╗██║  ██╗     ██████╔╝╚██████╔╝   ██║
+╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═════╝  ╚═════╝    ╚═╝
 */
 
 // basic mods
@@ -12,14 +12,14 @@ const fetch = require('node-fetch')
 // const _ = require('underscore');
 // const Chance = require('chance');
 // const chance = new Chance();
-const shuffle = require('shuffle-array');
+const shuffle = require('shuffle-array')
 
 // get secret token
 const fs = require('fs')
 let token = fs.readFileSync('./token.conf', 'utf8')
 if (!token) {
-    console.log('please set a slack token in a token.conf file')
-    return
+  console.log('please set a slack token in a token.conf file')
+  return
 }
 token = token.replace('\n', '')
 
@@ -28,8 +28,8 @@ const Botkit = require('botkit')
 let bot = Botkit.slackbot({ debug: false })
 bot.spawn({ token: token }).startRTM()
 
-/*                            __      ___    __       
-  |__| |  |  |\/|  /\  |\ | |  /  /\   |  | /  \ |\ | 
+/*                            __      ___    __
+  |__| |  |  |\/|  /\  |\ | |  /  /\   |  | /  \ |\ |
   |  | \__/  |  | /~~\ | \| | /_ /~~\  |  | \__/ | \| */
 /*
   Tools
@@ -37,7 +37,7 @@ bot.spawn({ token: token }).startRTM()
 const pick = (arr) => shuffle(arr)[0]
 const firstCap = (str) => str.charAt(0).toUpperCase() + str.slice(1)
 const withFun = true
-/* 
+/*
   Vocabulary
 */
 const endPunctuation = (withFun) => pick(withFun ? [' :stuck_out_tongue:', ' :p', ' :)', '', ' :smile:', ' :sunglasses:', ' :grin:'] : ['.', '.', '', '...'])
@@ -60,40 +60,40 @@ const goodBye = (userId) => say(byebye() + name(userId), withFun)
   Brain
 */
 const names = {
-    'U0760C9CL': 'Romain', // me
-    'U21PGPWE9': 'Romain',
-    'U0YBJJB0B': 'Léo',
-    'U0AMSTYCB': 'Bertrand',
-    'U23HBPVB2': 'Floris',
-    'U1Y7HMH1Q': 'Pauline',
-    'U0EQPACE6': 'Florian',
-    'U0YCZU9MY': 'Benjamin'
+  'U0760C9CL': 'Romain', // me
+  'U21PGPWE9': 'Romain',
+  'U0YBJJB0B': 'Léo',
+  'U0AMSTYCB': 'Bertrand',
+  'U23HBPVB2': 'Floris',
+  'U1Y7HMH1Q': 'Pauline',
+  'U0EQPACE6': 'Florian',
+  'U0YCZU9MY': 'Benjamin'
 }
 const name = (id) => (names[id] || '')
 
-/* ___  __        __       ___    __       
-  |__  |  \ |  | /  `  /\   |  | /  \ |\ | 
+/* ___  __        __       ___    __
+  |__  |  \ |  | /  `  /\   |  | /  \ |\ |
   |___ |__/ \__/ \__, /~~\  |  | \__/ | \|  */
 const context = ['mention', 'direct_mention']
 /*
   Hello
 */
 bot.hears('ça va', context, (bot, message) => {
-    if (message.text.indexOf('?') === -1) {
-        return
-    }
-    bot.reply(message, say(feelingGood() + 'et toi ?'))
+  if (message.text.indexOf('?') === -1) {
+    return
+  }
+  bot.reply(message, say(feelingGood() + 'et toi ?'))
 })
 /*
   Time
 */
 bot.hears('quelle heure', context, (bot, message) => {
-    if (message.text.indexOf('?') === -1) {
-        return
-    }
-    let date = new Date()
-    let time = date.getHours() + ':' + date.getMinutes()
-    bot.reply(message, say('il est ' + time))
+  if (message.text.indexOf('?') === -1) {
+    return
+  }
+  let date = new Date()
+  let time = date.getHours() + ':' + date.getMinutes()
+  bot.reply(message, say('il est ' + time))
 })
 /*
   Weather
@@ -101,36 +101,36 @@ bot.hears('quelle heure', context, (bot, message) => {
 let forecasts = {}
 fetch('http://www.prevision-meteo.ch/services/json/nantes').then(res => res.json()).then(json => forecasts = json)
 bot.hears(['quel temps', 'quel temp'], context, (bot, message) => {
-    if (message.text.indexOf('?') === -1) {
-        return
-    }
-    console.log('complete message was : ', message.text)
-    let today = (message.text.indexOf('demain') === -1)
-    let forecast = today ? forecasts.fcst_day_0 : forecasts.fcst_day_1
-    let response = today ? 'aujourd\'hui ' : 'demain '
-    response += 'il fera entre ' + forecast.tmin + '° et ' + forecast.tmax + '°'
-    bot.reply(message, say(response))
+  if (message.text.indexOf('?') === -1) {
+    return
+  }
+  console.log('complete message was : ', message.text)
+  let today = (message.text.indexOf('demain') === -1)
+  let forecast = today ? forecasts.fcst_day_0 : forecasts.fcst_day_1
+  let response = today ? 'aujourd\'hui ' : 'demain '
+  response += 'il fera entre ' + forecast.tmin + '° et ' + forecast.tmax + '°'
+  bot.reply(message, say(response))
 })
 /*
-  Welcome 
+  Welcome
 */
 bot.hears(greetings, context, (bot, message) => {
-    // console.log('got message', message)
-    bot.reply(message, welcome(message.user))
+  // console.log('got message', message)
+  bot.reply(message, welcome(message.user))
 })
 /*
   Polite
 */
 bot.hears(thanks, context, (bot, message) => {
-    // console.log('got message', message)
-    bot.reply(message, deNada(message.user))
+  // console.log('got message', message)
+  bot.reply(message, deNada(message.user))
 })
 /*
  Leave
 */
 bot.hears(byebyes, context, (bot, message) => {
-    // console.log('got message', message)
-    bot.reply(message, goodBye(message.user))
-    console.log('ok im leaving :p')
-    setTimeout(() => process.exit(0), 2000)
+  // console.log('got message', message)
+  bot.reply(message, goodBye(message.user))
+  console.log('ok im leaving :p')
+  setTimeout(() => process.exit(0), 2000)
 })
